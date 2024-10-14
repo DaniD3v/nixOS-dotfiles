1a9c310ede844d50fce177daf637613d74da696f
DaniD3v <sch220233@spengergasse.at>
2024-10-14 19:32:46 +0200

    fixed wal script & added asus auro support

--- nix/scripts/wal.nix
+++ nix/scripts/wal.nix
@@ -8,12 +8,11 @@
       export XDG_RUNTIME_DIR=/run/user/''${UID} # use right swww socket
 
       ${pkgs.swww}/bin/swww img ''${wallpaper} -t grow --transition-pos 0.7,0.9
-      
       ${pkgs.matugen}/bin/matugen image ''${wallpaper} 1>/dev/null
-      PRIMARY_COLOR=$(${pkgs.matugen}/bin/matugen image ''${wallpaper} --json strip" | ${pkgs.jq}/bin/jq ".colors.light.primary")
+      PRIMARY_COLOR=$(${pkgs.matugen}/bin/matugen image ''${wallpaper} --json strip | ${pkgs.jq}/bin/jq ".colors.light.primary" | ${pkgs.coreutils}/bin/tr -d '"')
 
       ${pkgs.pywalfox-native}/bin/pywalfox update 2>/dev/null
-      asusctl led-mode static -c "''$PRIMARY_COLOR" # asusctl is installed system-wide so I can't use an absolute path.
+      asusctl led-mode static -c "$PRIMARY_COLOR" # asusctl is installed system-wide so I can't use an absolute path.
     '';
 
     executable = true;
