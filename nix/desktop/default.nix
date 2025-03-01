501adb6783160aec28da1747cf38c65f4b42b787
DaniD3v <sch220233@spengergasse.at>
2025-03-01 06:42:34 +0100

    added hack to get hestia-bar working

--- nix/desktop/default.nix
+++ nix/desktop/default.nix
@@ -1,7 +1,11 @@
-_: {
+{pkgs, ...}: {
   imports = [
     ./hyprland.nix
     ./swaylock.nix
     ./swww.nix
   ];
+
+  # this is a hack.
+  # I will wrap this in hestia-bar later
+  home.packages = [pkgs.adwaita-icon-theme];
 }
