dac647562e4a86b198ce4022bb4d7a49715f4e93
DaniD3v <sch220233@spengergasse.at>
2025-03-01 06:46:16 +0100

    quoted wallpaper with possible spaces in it in script

--- nix/scripts/wal.nix
+++ nix/scripts/wal.nix
@@ -7,9 +7,9 @@
       wallpaper=$(${pkgs.coreutils}/bin/readlink -f "$1")
       export XDG_RUNTIME_DIR=/run/user/''${UID} # use right swww socket
 
-      ${pkgs.swww}/bin/swww img ''${wallpaper} -t grow --transition-pos 0.7,0.9
-      ${pkgs.matugen}/bin/matugen image ''${wallpaper} 1>/dev/null
-      PRIMARY_COLOR=$(${pkgs.matugen}/bin/matugen image ''${wallpaper} --json strip | ${pkgs.jq}/bin/jq -r ".colors.light.primary")
+      ${pkgs.swww}/bin/swww img "''${wallpaper}" -t grow --transition-pos 0.7,0.9
+      ${pkgs.matugen}/bin/matugen image "''${wallpaper}" 1>/dev/null
+      PRIMARY_COLOR=$(${pkgs.matugen}/bin/matugen image "''${wallpaper}" --json strip | ${pkgs.jq}/bin/jq -r ".colors.light.primary")
 
       ${pkgs.pywalfox-native}/bin/pywalfox update 2>/dev/null
       asusctl led-mode static -c "$PRIMARY_COLOR" # asusctl is installed system-wide so I can't use an absolute path.
